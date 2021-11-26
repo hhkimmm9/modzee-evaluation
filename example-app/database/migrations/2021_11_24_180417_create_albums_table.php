ce8dfc4adf1db4d4213129c4cf35b9f66c1f5387
Harrison Kim <hsookim90@gmail.com>
2021-11-26 13:59:44 -0500

    Completes migrations for profiles & albums and adds some sample data.

--- example-app/database/migrations/2021_11_24_180417_create_albums_table.php
+++ example-app/database/migrations/2021_11_24_180417_create_albums_table.php
@@ -16,16 +16,83 @@ class CreateAlbumsTable extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->foreignId('profile_id');
-            // $table->unsignedBigInteger('profile_id');
             $table->string('title');
             $table->string('description');
             $table->string('img');
             $table->date('date');
-            $table->boolean('featured');
+            $table->boolean('featured')->default(false);
             $table->timestamps();
 
             $table->index('profile_id');
         });
+
+        // Sample data - easy to rollback and test!
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "Nandhaka Pieris",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape1.jpg",
+                "2015-05-01",
+                true
+            ]
+        );
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "New West Calgary",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape2.jpg",
+                "2016-05-01",
+                false
+            ]
+        );
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "Australian Landscape",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape3.jpg",
+                "2015-02-02",
+                false
+            ]
+        );
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "Halvergate Marsh",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape4.jpg",
+                "2014-04-01",
+                true
+            ]
+        );
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "Rikkis Landscape",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape5.jpg",
+                "2010-09-01",
+                false
+            ]
+        );
+        DB::insert(
+            'INSERT INTO albums (profile_id, title, description, img, date, featured) VALUES (?, ?, ?, ?, ?, ?)',
+            [
+                1,
+                "Kiddi Kristjans Iceland",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img/landscape6.jpg",
+                "2015-07-21",
+                true
+            ]
+        );
     }
 
     /**
