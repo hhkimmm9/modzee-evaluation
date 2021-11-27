84c3efad633c805513f975a347b8bf70a43f69df
Harrison Kim <hsookim90@gmail.com>
2021-11-26 21:12:31 -0500

    Adds margin 0 padding 0 box-sizing: border-box.

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -5,6 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>RateMyPhotos</title>
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+        </style>
     </head>
     <body>
         <div id="app"></div>
