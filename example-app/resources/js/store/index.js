8828185684373f57211795baca19e51400187b93
Harrison Kim <hsookim90@gmail.com>
2021-11-27 18:29:33 -0500

    Adds a vuex module for albums.

--- example-app/resources/js/store/index.js
+++ example-app/resources/js/store/index.js
@@ -3,12 +3,14 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 
 import profile from "./modules/profile";
+import album from "./modules/album";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         profile,
+        album,
     },
     plugins: [createPersistedState()],
 });
---
+++ example-app/resources/js/store/modules/album.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const state = {
+    album: null,
+};
+
+const getters = {
+    getAlbum: (state) => state.album,
+};
+
+const actions = {
+    async fetchAlbumIndex({ commit }, id) {
+        await axios
+            .get(`http://localhost/api/album?whose=${id}`)
+            .then((res) => {
+                commit("setAlbum", res.data);
+            })
+            .catch((err) => alert(err));
+    },
+
+    clearAlbum({ commit }) {
+        commit("clearAlbum");
+    },
+};
+
+const mutations = {
+    setAlbum: (state, albumData) => (state.album = albumData),
+    clearAlbum: (state) => (state.album = null),
+};
+
+export default { state, getters, actions, mutations };
