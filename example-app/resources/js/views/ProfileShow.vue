c27379c823118ddd58d62d215aee12b6076f5ed7
Harrison Kim <hsookim90@gmail.com>
2021-11-28 12:52:18 -0500

    Adds media query for profileSection and combines some CSS properties.

--- example-app/resources/js/views/ProfileShow.vue
+++ example-app/resources/js/views/ProfileShow.vue
@@ -9,21 +9,19 @@
                 />
             </div>
             <div id="nameAndPersonalInfo">
-                <div id="upper">
-                    <h2>{{ getProfile.name }}</h2>
-                </div>
+                <h2>{{ getProfile.name }}</h2>
                 <div id="lower">
                     <div id="left">
-                        <h5 id="bio-title">Bio</h5>
-                        <p id="bio">{{ getProfile.bio }}</p>
+                        <h5 class="bottomZero">Bio</h5>
+                        <p id="bioContent">{{ getProfile.bio }}</p>
                     </div>
                     <div id="right">
-                        <h5 id="phone-title">Phone</h5>
-                        <p id="phone">
+                        <h5 class="bottomZero">Phone</h5>
+                        <p class="bottomZero fontColorPink">
                             {{ getProfile.phone }}
                         </p>
-                        <h5 id="email-title">Email</h5>
-                        <p id="email">
+                        <h5 class="bottomZero">Email</h5>
+                        <p class="bottomZero fontColorPink">
                             {{ getProfile.email }}
                         </p>
                     </div>
@@ -90,3 +88,89 @@ export default {
     display: flex;
     min-width: 768px;
 }
+
+#img {
+    flex: 1;
+    margin: auto;
+}
+
+#img > img {
+    width: 75%;
+    border-radius: 50%;
+}
+
+#nameAndPersonalInfo {
+    flex: 3;
+    text-align: left;
+    padding: 15px;
+}
+
+#lower {
+    display: flex;
+    color: darkgrey;
+}
+
+#bioContent {
+    margin-top: 0.5em;
+    margin-right: 0.5em;
+    color: lightgray;
+}
+
+.bottomZero {
+    margin: 0.3em 0;
+}
+
+.fontColorPink {
+    color: hotpink;
+}
+
+#albumSection {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 25px;
+}
+
+@media (max-width: 800px) {
+    #profileSection {
+        background: white;
+        width: 50%;
+        margin: 1em auto;
+        display: flex;
+        flex-direction: column;
+        min-width: 200px;
+    }
+
+    #img {
+        margin: 15px auto;
+    }
+
+    #img > img {
+        width: 50%;
+        border-radius: 50%;
+    }
+
+    #nameAndPersonalInfo {
+        text-align: center;
+        padding: 1.5em;
+    }
+
+    #lower {
+        display: flex;
+        flex-direction: column;
+        color: darkgrey;
+    }
+
+    #left {
+    }
+
+    #right {
+    }
+
+    #bioContent {
+        margin: 0.5em auto;
+        color: lightgray;
+        text-align: justify;
+    }
+}
+</style>
